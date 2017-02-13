a54b7e045cb48dd491d48ce19a15ac864e3f86a8
thawisak <kunggong87@gmail.com>
2017-02-13 21:18:23 +0700

    lab4-5

---
+++ lab4_1/src/lab4_5/SingleObject.java
@@ -0,0 +1,13 @@
+
+package lab4_5;
+
+
+public class SingleObject {
+    SingleObject instance;
+    public SingleObject(){
+     }
+    public SingleObject getlnstance(){
+     }
+    public void show message(){
+     }
+}
---
+++ lab4_1/src/lab4_5/SingletonPatternDemo.java
@@ -0,0 +1,8 @@
+
+package lab4_5;
+
+
+public class SingletonPatternDemo {
+    public static void main(String[] args){
+    } 
+}
